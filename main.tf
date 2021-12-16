ed5ac56773f51120be526afcd477c2d6455791b4
fultux <flavien.rugiano@suse.com>
2021-12-16 18:21:43 +0100

    progress (or not)

--- main.tf
+++ main.tf
@@ -1,14 +1,46 @@
+
+module "network" { 
+	source 	=  "./modules/network"
+	subnet = var.subnet
+	subnet_mask = var.subnet_mask
+	lb_name = var.lb_name
+	lb_ip = var.lb_ip
+		providers = {
+	  name = libvirt.main
+	 }
+}
 module "kvm" {
-	source	 =	"./modules/kvm"
+	source	 = "./modules/kvm"
 	main_count	= var.main_count
 	main_node_memory = var.main_node_memory
 	main_node_vcpu	= var.main_node_vcpu
 	node_count	= var.node_count
 	worker_node_memory	=	var.worker_node_memory
 	worker_node_vcpu	=	var.worker_node_vcpu
-	node_ssh_key	=	var.node_ssh_key
-	node_ssh_user	=	var.node_ssh_user
+	node_ssh_key	=	var.ssh_key
+	node_ssh_user	=	var.ssh_user
+	depends_on = [module.network]
+	providers = {
+	  name = libvirt.main
+	 }
 }
+	
+
+module "nginx" {
+	source = "./modules/nginx"
+	lb_memory = var.lb_memory
+	lb_vcpu = var.lb_vcpu
+	lb_ssh_key = var.ssh_key
+	lb_ssh_user = var.ssh_user
+	worker_ip = var.worker_ip
+	lb_name = var.lb_name
+	lb_ip = var.lb_ip
+	depends_on = [module.kvm]
+	providers = {
+	  name = libvirt.main
+	 }
+	}
+
 
 
 
@@ -19,3 +51,5 @@ module "kvm" {
 	ssh_key_file = var.ssh_key_file
 	depends_on = [module.kvm]
 }
+
+
